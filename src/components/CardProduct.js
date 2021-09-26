cc1c19cc628d767287b4f95500d8aa81bde3458c
hoanglam16tg <hoanglam16tg@gmail.com>
2021-09-26 15:23:54 +0700

    up source

--- src/components/CardProduct.js
+++ src/components/CardProduct.js
@@ -5,7 +5,7 @@ const CardProduct = (props) => {
   return (
     <div className="card">
       <div className="wrap__content">
-        <img src={`https://api.autoxanh.com/${srcImage}`} alt="" />
+        <img src={`https://api.autoxanh.com${srcImage}`} alt="" />
         <h4>{title}</h4>
         <p>{description}</p>
         <h5>Price: {price} </h5>
